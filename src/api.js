43c59f4ede5ec537d8459246060504c0f60fd24a
hassan0341 <hassan200354@outlook.com>
2024-09-23 12:23:26 +0100

    Implement axios instance

--- src/api.js
+++ src/api.js
@@ -1,8 +1,12 @@
 import axios from "axios";
 
+const api = axios.create({
+  baseURL: "https://my-back-end-project.onrender.com/api",
+});
+
 function getArticles(topic, sortBy, orderBy) {
-  return axios
-    .get("https://my-back-end-project.onrender.com/api/articles", {
+  return api
+    .get("/articles", {
       params: {
         topic: topic,
         sort_by: sortBy,
@@ -18,8 +22,8 @@ function getArticles(topic, sortBy, orderBy) {
 }
 
 function getArticleById(article_id) {
-  return axios
-    .get(`https://my-back-end-project.onrender.com/api/articles/${article_id}`)
+  return api
+    .get(`/articles/${article_id}`)
     .then((response) => {
       return response.data.article;
     })
@@ -29,57 +33,41 @@ function getArticleById(article_id) {
 }
 
 function getCommentsByArticleID(article_id) {
-  return axios
-    .get(
-      `https://my-back-end-project.onrender.com/api/articles/${article_id}/comments`
-    )
-    .then((response) => {
-      return response.data.comments;
-    });
+  return api.get(`/articles/${article_id}/comments`).then((response) => {
+    return response.data.comments;
+  });
 }
 
 function patchVoteByArticleId(article_id, vote) {
-  return axios
-    .patch(
-      `https://my-back-end-project.onrender.com/api/articles/${article_id}`,
-      { inc_votes: vote }
-    )
+  return api
+    .patch(`/articles/${article_id}`, { inc_votes: vote })
     .then((response) => {
       return response.data;
     });
 }
 
 function postComment(article_id, commentData) {
-  return axios
-    .post(
-      `https://my-back-end-project.onrender.com/api/articles/${article_id}/comments`,
-      commentData
-    )
+  return api
+    .post(`/articles/${article_id}/comments`, commentData)
     .then((response) => {
       return response.data;
     });
 }
 
 function getUsernames() {
-  return axios
-    .get("https://my-back-end-project.onrender.com/api/users")
-    .then((response) => {
-      return response.data.users;
-    });
+  return api.get("/users").then((response) => {
+    return response.data.users;
+  });
 }
 
 function deleteComment(comment_id) {
-  return axios.delete(
-    `https://my-back-end-project.onrender.com/api/comments/${comment_id}`
-  );
+  return api.delete(`/comments/${comment_id}`);
 }
 
 function getTopics() {
-  return axios
-    .get("https://my-back-end-project.onrender.com/api/topics")
-    .then((response) => {
-      return response.data.topics;
-    });
+  return api.get("/topics").then((response) => {
+    return response.data.topics;
+  });
 }
 
 export {
